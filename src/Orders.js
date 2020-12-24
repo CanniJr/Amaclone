1624584940209b05e179b2aac762ff52dbc43d54
CanniJr <cannigialaluw@gmail.com>
2020-12-23 22:50:59 -0500

    Processing user payment in Firebase

--- src/Orders.js
+++ src/Orders.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './css/Orders.css'
+import { useStateValue } from './StateProvider';
 
 function Orders() {
+    const [state, dispatch] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        
+    }, [])
+
     return (
         <div className='orders'>
             <h1>Orders here</h1>
